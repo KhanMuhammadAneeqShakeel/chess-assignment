b229e6e180a23211b319a2a0ffe5f558a6ae2e6e
unknown <Aneeq.KHAN@student.howest.be>
2026-02-28 16:43:59 +0100

    feat: add Rook and Bishop classes

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("aneeq")
+from chess.pieces import Pawn
+from chess.pieces import Pawn, Rook, Bishop
+
+pawn1 = Pawn("BLACK", 1)
+pawn1.move()
+
+print(pawn1)
+
+
+pawn1 = Pawn("BLACK", 1)
+rook1 = Rook("WHITE", 1)
+bishop1 = Bishop("BLACK", 2)
+
+pawn1.move()
+rook1.move()
+bishop1.move()
+
+print(pawn1)
+print(rook1)
+print(bishop1)
